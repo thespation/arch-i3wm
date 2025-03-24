69a0301392f84b2a22087bf887482b82b5804c90
William Santos <thespation@gmail.com>
2025-03-24 08:40:27 -0300

    Add files via upload

---
+++ config/i3/polybar/scripts/troca_config_verificacao.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Caminho para o arquivo de configuração
+CONFIG_FILE="$HOME/.config/i3/polybar/config.ini"
+
+# Verifica qual linha está comentada e ajusta de acordo
+if grep -q "^#modules-left = launcher i3 term files browser theme cpuBar" "$CONFIG_FILE"; then
+    # Se a primeira linha estiver comentada, descomenta ela e comenta a segunda
+    sed -i \
+        -e '/^#modules-left = launcher i3 term files browser theme cpuBar$/s/^#//' \
+        -e '/^modules-left = launcher i3_numbers term files browser theme cpuBar$/s/^/#/' \
+        "$CONFIG_FILE"
+else
+    # Caso contrário, comenta a primeira linha e descomenta a segunda
+    sed -i \
+        -e '/^modules-left = launcher i3 term files browser theme cpuBar$/s/^/#/' \
+        -e '/^#modules-left = launcher i3_numbers term files browser theme cpuBar$/s/^#//' \
+        "$CONFIG_FILE"
+fi
+
+# Reinicia o i3wm para aplicar as mudanças
+i3-msg restart
+
+echo "Troca realizada e configurações recarregadas com sucesso!"
