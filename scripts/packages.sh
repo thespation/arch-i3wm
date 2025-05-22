a3f64aabb9156ee4e8ea614d387c5612fc5d5f52
William Santos <thespation@gmail.com>
2025-05-22 10:24:03 -0300

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -51,7 +51,7 @@ install_packages() {
 install_yay() {
   if ! command -v yay &>/dev/null; then
     echo -e "${YELLOW}Instalando yay...${NC}"
-    git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
+    git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin &>/dev/null
     cd /tmp/yay-bin && makepkg -si --noconfirm &>/dev/null
     cd - &>/dev/null
     if command -v yay &>/dev/null; then
